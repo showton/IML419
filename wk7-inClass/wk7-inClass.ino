b52644502729d166fec0f2ccd60e5756fc8a30db
Sylvie Howton <sylviehowton@usc-securewireless-student-new191.usc.edu>
2019-02-25 09:38:15 -0800

    edits to week 5, added week 6 in class

--- wk7-inClass/wk7-inClass.ino
+++ wk7-inClass/wk7-inClass.ino
@@ -1,25 +1,25 @@
-int pinArray[3]={13, 12, 4};
+int pinArray[5]={13, 12, 7, 4, 2};
 //int timer = 100;
-int potPin = A0;
+int phoCel = A0;
 int i = 0;
 int sensVal = 0;
 
 
 
 void setup(){
-for (i=0; i<3; i++) {
+for (i=0; i<5; i++) {
     pinMode(pinArray[i], OUTPUT);
   }
 }
 
 void loop(){
-sensVal = analogRead(potPin);
- for (i=0;i<3;i++) {
+sensVal = analogRead(phoCel);
+ for (i=0;i<5;i++) {
    digitalWrite(pinArray[i], HIGH);
    delay(sensVal);
   }
   
-  for (i=3;i>=0;i--) {
+  for (i=4;i>=0;i--) {
    digitalWrite(pinArray[i], LOW);
    delay(sensVal);
   }
